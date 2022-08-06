842c8518c239116525e8da02880211d049ab0e64
Mohamed Rhimi <76263599+MRhimi@users.noreply.github.com>
2022-08-07 00:18:19 +0100

    "Pdf to Single Image should be less than 20 pages"

--- iOSMultiPdfToImage/ViewController.swift
+++ iOSMultiPdfToImage/ViewController.swift
@@ -35,26 +35,43 @@ class ViewController: UIViewController {
     
     func drawPDFfromURL() -> UIImage? {
         
-        let filePath = Bundle.main.path(forResource: "Scanned-Docs", ofType: "pdf")!
-      
+        let filePath = Bundle.main.path(forResource: "iOSDevelopment", ofType: "pdf")!
+        
         let url = URL(fileURLWithPath: filePath)
         
         guard let document = CGPDFDocument(url as CFURL) else { return nil }
-        guard let page = document.page(at: 1) else { return nil }
-
-        let pageRect = page.getBoxRect(.mediaBox)
-        let renderer = UIGraphicsImageRenderer(size: pageRect.size)
-        let img = renderer.image { ctx in
-            UIColor.white.set()
-            ctx.fill(pageRect)
-
-            ctx.cgContext.translateBy(x: 0.0, y: pageRect.size.height)
-            ctx.cgContext.scaleBy(x: 1.0, y: -1.0)
-
-            ctx.cgContext.drawPDFPage(page)
+       // guard let page = document.page(at: 1) else { return nil }
+        
+        var width: CGFloat = 0
+        var height: CGFloat = 0
+        
+        // calculating overall page size
+        for index in 1...document.numberOfPages {
+            print("index: \(index)")
+            if let page = document.page(at: index) {
+                let pageRect = page.getBoxRect(.mediaBox)
+                width = max(width, pageRect.width)
+                height = height + pageRect.height
+            }
         }
         
-        return img
+        // now creating the image
+        let renderer = UIGraphicsImageRenderer(size: CGSize(width: width, height: height))
+        
+        let image = renderer.image { (ctx) in
+            ctx.cgContext.scaleBy(x: 1.0, y: -1.0)
+            for index in 1...document.numberOfPages {
+                
+                if let page = document.page(at: index) {
+                    let pageRect = page.getBoxRect(.mediaBox)
+                    ctx.cgContext.translateBy(x: 0.0, y: -pageRect.height)
+                    ctx.cgContext.drawPDFPage(page)
+                }
+            }
+            
+            ctx.cgContext.scaleBy(x: 1.0, y: -1.0)
+        }
+        return image
     }
     
 }
